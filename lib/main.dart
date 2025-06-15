8cbc591b5c6b15e39cc21f8122de0206d6f90cf7
F6323258 Lucas Pereira Gomes Linard <154244432+lpglinard@users.noreply.github.com>
2025-06-14 22:49:03 -0300

    Add Firebase configuration files for Android and iOS

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'l10n/app_localizations.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const ProviderScope(child: MyApp()));
 }
 
