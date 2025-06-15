f8fe118ce53d8702ac3f69d04caca8a16c6430f4
lpglinard <154244432+lpglinard@users.noreply.github.com>
2025-06-14 22:54:08 -0300

    Set NDK and minSdk versions

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,7 +11,8 @@ plugins {
 android {
     namespace = "tech.linard.travelgenieapp.travel_genie"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    // Explicitly set the Android NDK version to satisfy Firebase dependencies
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -27,7 +28,8 @@ android {
         applicationId = "tech.linard.travelgenieapp.travel_genie"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        // Firebase Auth requires at least API level 23
+        minSdk = 23
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
